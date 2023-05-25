518e05b1780d436f58d097b1bafbdce87d350c20
fanny_andreina <fa.araujo@duocuc.cl>
2023-05-24 20:37:46 -0400

    tercer envio

--- holamundo.py
+++ holamundo.py
@@ -6,3 +6,6 @@ print(f"{mensaje}")
 # git commit -m "nombre para el archivo"
 # git config --global user.name "firts_name_last_name"
 #git config --global user.email "fa.araujo@duocuc.cl"
+# git fetch sirve para actualizar
+# git pull actualiza desde la nube
+#git push - te pide que te logee
