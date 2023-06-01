0b032174c4a8d081d74aa99e3302dd11b35854c6
fnny araujo <fa.araujo@duocuc.cl>
2023-06-01 19:43:36 -0400

    envio 1-6

--- holamundo.py
+++ holamundo.py
@@ -3,7 +3,7 @@ print(f"{mensaje}")
 #git status
 # clear
 # git add . sirve para garegar a la carpeta que esta oculta
-# git commit -m "nombre para el archivo"
+# git commit -m  "nombre para el archivo"
 # git config --global user.name "firts_name_last_name"
 #git config --global user.email "fa.araujo@duocuc.cl"
 # git fetch sirve para actualizar
