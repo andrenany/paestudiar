ce8864b68428a1d1362ecf4ce6090d700418bf5d
Christian_Cardenas <ch.cardenasi@duocuc.cl>
2023-06-03 14:10:09 -0400

    nombre para el archiv2o

---
+++ clasesabado_ejercicio1.py.py
@@ -0,0 +1,8 @@
+#crea un arreglo unidimensional con nombre arregloA y de tamaño 100 con numeros aleatorios de numeros enteros de 0 al 500, luego:
+import numpy as np
+arregloA=np.random.randint(500, size=(100)) 
+print(arregloA)
+print("Mostrar por pantalla sólo los valores que se encuentren en los índices pares del arreglo.")
+print(arregloA[::2])
+print("Mostrar el elemento mayor del arreglo", arregloA.max())
+print("Mostrar el índice (posición) del elemento mayor:", arregloA.max.index())
